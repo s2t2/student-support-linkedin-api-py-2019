2d4a2bd418b3289b21b29126abea93a6e8e114d5
Mike Rossetti <s2t2@users.noreply.github.com>
2019-06-27 02:30:55 -0400

    Got my profile info, not much there

--- get_profile.py
+++ get_profile.py
@@ -1,4 +1,5 @@
 import os
+from pprint import pprint
 from dotenv import load_dotenv
 from linkedin_v2 import linkedin
 import webbrowser
@@ -7,6 +8,10 @@ load_dotenv()
 
 LINKEDIN_ACCESS_TOKEN = os.environ.get("LINKEDIN_ACCESS_TOKEN")
 
+#PERMISSIONS = linkedin.PERMISSIONS.enums.values() # trying to use all these permissions leads to errors
+#> dict_values(['rw_company_admin', 'r_basicprofile', 'r_fullprofile', 'r_emailaddress', 'r_network', 'r_contactinfo', 'rw_nus', 'rw_groups', 'w_messages'])
+PERMISSIONS = ["r_emailaddress", "r_liteprofile", "w_member_social"] # ["r_basicprofile"]
+
 def user_get_token():
     #
     # GET AN AUTH CODE
@@ -14,11 +19,9 @@ def user_get_token():
     CLIENT_ID = os.environ.get("LINKEDIN_CLIENT_ID", "OOPS")
     CLIENT_SECRET = os.environ.get("LINKEDIN_CLIENT_SECRET", "OOPS")
     REDIRECT_URL = os.environ.get("LINKEDIN_REDIRECT_URL", "http://localhost:8000")
-    PERMISSIONS = linkedin.PERMISSIONS.enums.values()
     #print(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL)
-    #print(PERMISSIONS) #> dict_values(['rw_company_admin', 'r_basicprofile', 'r_fullprofile', 'r_emailaddress', 'r_network', 'r_contactinfo', 'rw_nus', 'rw_groups', 'w_messages'])
 
-    auth = linkedin.LinkedInAuthentication(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL, ["r_basicprofile"])
+    auth = linkedin.LinkedInAuthentication(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL, PERMISSIONS)
     #print(type(auth)) #> <class 'linkedin_v2.linkedin.LinkedInAuthentication'>
     #print(auth.authorization_url) # opening this url in the browser. seeing unauthorized_scope_error...
 
@@ -36,7 +39,7 @@ def user_get_token():
     # GET AN ACCESS TOKEN
     #
 
-    access_token = auth.get_access_token()
+    access_token = auth.get_access_token() # this is an AccessToken object with a access_token property, which should be stored in the env var
 
     return access_token
 
@@ -52,5 +55,26 @@ if __name__ == "__main__":
 
     print("----------------")
     print("INITIALIZING APP...")
-    app = linkedin.LinkedInApplication(token=token)
-    print(type(app))
+    client = linkedin.LinkedInApplication(token=token)
+    print(type(client))
+
+    print("----------------")
+    print("GETTING PROFILE...")
+    profile = client.get_profile()
+    pprint(profile)
+
+    #>{
+    #>    'firstName': {
+    #>        'localized': {'en_US': 'Polly'},
+    #>        'preferredLocale': {'country': 'US', 'language': 'en'}
+    #>    },
+    #>    'id': '987zyx',
+    #>    'lastName': {
+    #>        'localized': {'en_US': 'Professor'},
+    #>        'preferredLocale': {'country': 'US', 'language': 'en'}
+    #>    },
+    #>    'localizedFirstName': 'Polly',
+    #>    'localizedLastName': 'Professor',
+    #>    'numConnections': 0,
+    #>    'profilePicture': {'displayImage': 'urn:li:digitalmediaAsset:456plk'}
+    #>}
